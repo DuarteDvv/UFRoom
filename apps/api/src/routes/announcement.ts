6185c8c53369e804360a1691cc417211e4fccbd7
DuarteDvv <luisduarte.dev@gmail.com>
2025-09-02 17:33:43 -0300

    Update announcement routes to use consistent naming conventions

--- apps/api/src/routes/announcement.ts
+++ apps/api/src/routes/announcement.ts
@@ -5,8 +5,11 @@ import * as announcementController from "../controllers/announcement";
 
 export default async function (fastify: FastifyInstance) {
 
-    fastify.post("/c-a", announcementController.createAnnouncement);
-    fastify.get("/g-a", announcementController.getAllAnnouncements);
-    
+    fastify.post("/announcement", announcementController.createAnnouncement);
+    fastify.get("/announcement", announcementController.getAllAnnouncements);
+    fastify.get("/announcement/:id", announcementController.getAnnouncementById);
+    fastify.put("/announcement/:id", announcementController.updateAnnouncement);
+    fastify.delete("/announcement/:id", announcementController.deleteAnnouncement);
+
 }
 
