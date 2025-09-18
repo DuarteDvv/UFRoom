9534bc3bb4aa3e72e831f5dc111a83c0d74dd427
MarcusMoraesOliveira <marcussviniciusss123@gmail.com>
2025-09-17 23:06:06 -0300

    feature: add user login route

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -5,6 +5,7 @@ import cors from '@fastify/cors'
 import helmet from '@fastify/helmet'
 import rateLimit from '@fastify/rate-limit'
 import prismaPlugin from './plugins/prisma' 
+import fastifySensible from "fastify-sensible";
 
 import addressRoutes from './routes/address'
 import authRoutes from './routes/auth'
@@ -78,6 +79,8 @@ async function createServer(options: ServerOptions = {}) {
   
   // Registrando o plugin do Prisma
   await server.register(prismaPlugin)
+
+  await server.register(fastifySensible)
 
   // Registrando rotas de endereço
   await server.register(addressRoutes)
