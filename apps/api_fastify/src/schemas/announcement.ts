4e03b66b60e910a7e635b8b31d2023741e303083
Gustavo Paiva <gust4vo.dev@gmail.com>
2025-09-27 19:56:31 -0300

    announcement schema

--- apps/api_fastify/src/schemas/announcement.ts
+++ apps/api_fastify/src/schemas/announcement.ts
@@ -1,7 +1,38 @@
-import { Type, Static } from "@sinclair/typebox";
+import { Type, Static } from '@sinclair/typebox'
 
-export const AnnouncementSchema = Type.Object({
+const PropertyType = Type.Union([
+  Type.Literal('kitnet'),
+  Type.Literal('individual_room'),
+  Type.Literal('shared_room')
+])
 
-});
+const AnnouncementStatus = Type.Union([
+  Type.Literal('paused'),
+  Type.Literal('rented'),
+  Type.Literal('available')
+])
 
-export type Announcement = Static<typeof AnnouncementSchema>;
+const SexRestrict = Type.Union([
+  Type.Literal('male'),
+  Type.Literal('female'),
+  Type.Literal('both')
+])
+
+export const Announcement = Type.Object({
+  id: Type.Optional(Type.Number()),
+  id_owner: Type.Number(),
+  id_address: Type.Optional(Type.Number()),
+  price: Type.Number({ minimum: 0 }),
+  occupants: Type.Number({ minimum: 0 }),
+  max_occupants: Type.Number({ minimum: 1 }),
+  description: Type.Optional(Type.String()),
+  type_of: PropertyType,
+  status: AnnouncementStatus,
+  title: Type.String({ maxLength: 255 }),
+  sex_restriction: SexRestrict,
+  created_at: Type.String({ format: 'date-time' }),
+  updated_at: Type.String({ format: 'date-time' }),
+  rules: Type.Optional(Type.String())
+})
+
+export type AnnouncementType = Static<typeof Announcement>
